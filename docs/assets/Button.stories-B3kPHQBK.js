ebf5c0b11f0e88e6efb342becf91382054faa5e4
abdelrhman-adel <abdelrhman.adel@rangle.io>
2024-06-18 08:17:03 +0000

    Update from https://github.com/dfds-frontend/dfds-unified-web/commit/9d3a290de2f34c6221518c29119387d922c09713

---
+++ docs/assets/Button.stories-B3kPHQBK.js
@@ -0,0 +1,5 @@
+import{B as a}from"./Button-DKioIsLR.js";import"./index-Cd-xqqnJ.js";import"./index-DVXBtNgz.js";import"./index-DPx8FGzu.js";const p={title:"Atoms/Button",component:a,parameters:{layout:"centered"},tags:["autodocs"]},r={args:{children:"Button"}};var t,o,e;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    children: 'Button'
+  }
+}`,...(e=(o=r.parameters)==null?void 0:o.docs)==null?void 0:e.source}}};const i=["Primary"];export{r as Primary,i as __namedExportsOrder,p as default};
